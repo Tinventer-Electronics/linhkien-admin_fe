db0322be90f2c2cd75828ce742f974cd45edc26b
QuangND <quang5203@gmail.com>
2025-03-25 17:45:39 +0700

    add refreshToken

--- src/components/HeaderComponent/index.jsx
+++ src/components/HeaderComponent/index.jsx
@@ -5,20 +5,48 @@ import { FiSearch } from 'react-icons/fi';
 import { MdNotificationsNone } from 'react-icons/md';
 import { useDispatch } from 'react-redux';
 import { removeAuth } from '../../redux/reducers/authReducer';
+// import handleAPI from '../../api/handleApi';
+// import { apiEndpoint } from '../../constants/apiEndpoint';
+// import { useSelector } from 'react-redux';
+// import { authSelector, refreshToken } from '../../redux/reducers/authReducer';
 
 const HeaderComponent = () => {
     const dispatch = useDispatch();
+    // const auth = useSelector(authSelector);
+    const logout = () => {
+        dispatch(removeAuth());
+        localStorage.removeItem('authData');
+    };
+
+    // const getProducts = async () => {
+    //     try {
+    //         const res = await handleAPI(apiEndpoint.product.getAll);
+    //         console.log(res);
+    //     } catch (error) {
+    //         console.log(error);
+    //         if (error.error === 'jwt expired') {
+    //             handleRefreshToken();
+    //         }
+    //     }
+    // };
+
+    // const handleRefreshToken = async () => {
+    //     try {
+    //         const res = await handleAPI(`${apiEndpoint.auth.refreshToken}?id=${auth._id}`);
+    //         dispatch(refreshToken(res.data.token));
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // };
 
     const items = [
         {
             key: 'logout',
             label: 'Đăng xuất',
-            onClick: () => {
-                dispatch(removeAuth());
-                localStorage.removeItem('authData');
-            },
+            onClick: logout,
         },
     ];
+
     return (
         <div className="p-2 grid grid-cols-2 bg-white">
             <div>
--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -18,9 +18,12 @@ const authSlide = createSlice({
                 _id: '',
             };
         },
+        refreshToken: (state, action) => {
+            state.data.token = action.payload;
+        },
     },
 });
 
 export const authReducer = authSlide.reducer;
-export const { addAuth, removeAuth } = authSlide.actions;
+export const { addAuth, removeAuth, refreshToken } = authSlide.actions;
 export const authSelector = (state) => state.authReducer.data;
